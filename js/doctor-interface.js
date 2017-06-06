f64a355e2ab156cc92bf7694bdb9ce2c818b6253
kaytlinrb <kaytlinrb@gmail.com>
2017-06-06 15:38:02 -0700

    wheeeeeeeeeeeee

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -8,20 +8,20 @@ var displayDoctors = function(issue, doctorData){
   doctorData.forEach(function(doctor){
     var firstname = doctor.profile.first_name;
     var lastname = doctor.profile.last_name;
-    $('#doctorPannel').append(firstname + " " + lastname);
+    $('#doctorPannel').append(firstname + " " + lastname + "<br>");
   });
 
 };
 
 $(document).ready(function(){
   var newDoc = new Doctor();
-$('#medicalForm').submit(function(){
-  var issue = $('#medicalIssueInput').val();
-  $('#medicalIssueInput').val("");
-  $('#resultsDiv').show();
+  $('#medicalForm').submit(function(event){
+    event.preventDefault();
+    var issue = $('#medicalIssueInput').val();
+    $('#medicalIssueInput').val("");
+    $('#resultsDiv').show();
 
-  console.log(newDoc.getDoctors(issue, displayDoctors))
 
-  newDoc.getDoctors(issue, displayDoctors);
+    newDoc.getDoctors(issue, displayDoctors);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -2,14 +2,16 @@ var apiKey = require('./../.env').apiKey;
 
 
 Doctor = function(){
+
 };
 
 Doctor.prototype.getDoctors = function(issue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ issue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
-      console.log(response);
+      console.log(JSON.stringify(response));
+      console.log(response.data.length);
     displayDoctors(issue, response.data);
 
-  })
+  });
 
 };
 
